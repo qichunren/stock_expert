5cdf53a3d6eeafe2dfc854810857e285c41c9d82
Qichunren 曹金华 <whyruby@gmail.com>
2011-09-08 06:41:03 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StockExpert::Application.config.secret_token = 'dc1012b3a0bf1bf2eb5dfb085f502f5f5ec2ce7eae3cb0a65b0d566f8c41fa6f88ab78014c5ca34a5fd7bdc9f6d7521c1387819ae2c3a0ea3d346e5597dc747b'
